4d31dd473f06dcfea1614a619c177dff83a87d96
park se won <tpdnjsdkwk@naver.com>
2023-12-04 01:57:34 +0900

    docs

---
+++ DefaultWindow/ItemBox.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "ItemBox.h"
+
+ItemBox::ItemBox()
+{
+}
+
+ItemBox::~ItemBox()
+{
+}
+
+void ItemBox::Initialize()
+{
+}
+
+int ItemBox::Update()
+{
+    return 0;
+}
+
+void ItemBox::Late_Update()
+{
+}
+
+void ItemBox::Render(HDC hDC)
+{
+}
+
+void ItemBox::Release()
+{
+}
